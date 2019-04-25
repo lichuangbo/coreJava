ce64c748cec66edfe60ff6d4ffe2ddbbf4fdfd36
xiaotian <15503609389@163.com>
2019-04-25 22:36:50 +0800

    Add .txt and .jpg file version 1.3

--- net/UploadClient.java
+++ net/UploadClient.java
@@ -1,8 +1,9 @@
 /**
- * CopyRight 2019/4/16
+ * CopyRight 2019/4/25
  */
 package cn.edu.tit.corejava.socket;
 
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,36 +15,60 @@ import java.net.Socket;
 
 /**
  * @author 李创博
- * @version 1.2
+ * @version 1.3
  */
 public class UploadClient {
 
 	/**文件上传的客户端：读取本地文件，上传到服务器，读取服务器回写的数据
-	 * 文件类型是文本类型
+	 * 文件可以是text文件/jpg图片
 	 * @throws IOException  
 	 */
 	public static void main(String[] args) throws IOException {
 		/**流程：
-		 * 1.创建FileReader对象，绑定数据源
-		 * 2.创建客户端Socket对象，指定IP和端口号
-		 * 3.获取OutputStream对象
-		 * 4.使用FileReader对象读取本地文件
-		 * 5.使用OutputStream对象将读到的文件写入指定服务器
-		 * 
-		 * 6.获取InputStream对象
-		 * 7.使用InputStream对象读取服务器回复的数据
-		 * 8.释放资源
-		 * 源文件：21199字节
-		 * 测试上传文件共用时：22ms
+		 * 1.创建客户端Socket对象，指定IP和端口号
+		 * 2.根据路径信息判断文件类型，将文件类型发送给服务器端
+		 * 3.根据不同的文件类型选择不同的上传方式
+		 * 4.创建FileReader/FileInputStream对象，绑定数据源
+		 * 5.获取OutputStream对象
+		 * 6.使用FileReader/FileInputStream对象读取本地文件
+		 * 7.使用OutputStream对象将读到的文件写入指定服务器
+		 * 8.获取InputStream对象
+		 * 9.使用InputStream对象读取服务器回复的数据
+		 * 10.释放资源
+		 * 测试图片：D:\java11\Zhang\src\java_classes\女.jpg
+		 * 测试文件：D:\java11\Zhang\src\java_classes\FileInputStreamDemo.txt
 		 * */
-		//开始上传文件时间
-		long start = System.currentTimeMillis();
-		
-		FileReader fr = new FileReader("D:\\java11\\Zhang\\src\\java_classes\\FileInputStreamDemo.txt");
 		
 		Socket socket = new Socket("127.0.0.1", 8000);
 		
+		String filePath = "D:\\java11\\Zhang\\src\\java_classes\\女.jpg";
+//		String filePath = "D:\\java11\\Zhang\\src\\java_classes\\FileInputStreamDemo.txt";
+
+		String type = filePath.split("\\.")[1];
+		
 		OutputStream os = socket.getOutputStream();
+		if(type.equals("jpg")) {
+			//规定1表示是二进制类型文件，0表示是文本类型文件
+			os.write(1);
+			uploadUnTextFile(socket, filePath);
+		}else if(type.equals("txt")) {
+			os.write(0);
+			uploadTextFile(socket, filePath);
+		}		
+		os.close();
+	}
+	
+	/**
+	 * 上传文本类型文件
+	 * @param s Socket对象
+	 * @param filePath 文件路径名
+	 * @throws IOException
+	 * @return: void
+	 */
+	public static void uploadTextFile(Socket s, String filePath) throws IOException {
+		FileReader fr = new FileReader(filePath);
+		
+		OutputStream os = s.getOutputStream();
 		OutputStreamWriter osw = new OutputStreamWriter(os);
 		
 		int len = 0;
@@ -53,21 +78,47 @@ public class UploadClient {
 		}
 		
 		osw.flush();//关键
-		socket.shutdownOutput();// 防止readLine方法阻塞
+		s.shutdownOutput();// 防止readLine方法阻塞
 		
-		InputStream is = socket.getInputStream();
+		InputStream is = s.getInputStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		while((len = isr.read(temp)) != -1) {
 			System.out.println(new String(temp, 0, len));
-		}		
+		}
 		
-		//文件上传完毕时间
-		long end = System.currentTimeMillis();
-		System.out.println("上传文件共耗时：" + (end - start) + "毫秒");
-		
-		osw.close();
 		fr.close();
-		socket.close();
+		s.close();
 	}
-
+	
+	/**
+	 * 上传非文本类型文件
+	 * @param s Socket对象
+	 * @param filePath 文件路径名
+	 * @return: void
+	 * @throws IOException 
+	 */
+	public static void uploadUnTextFile(Socket s, String filePath) throws IOException {
+		FileInputStream fis = new FileInputStream(filePath);
+		
+		OutputStream os = s.getOutputStream();
+		
+		byte []temp = new byte[1024];
+		int len = 0;
+		while((len = fis.read(temp)) != -1) {
+			os.write(temp, 0, len);
+		}
+		
+		os.flush();
+		s.shutdownOutput();
+		
+		InputStream is = s.getInputStream();
+		while((len = is.read(temp)) != -1) {
+			System.out.println(new String(temp, 0, len));
+		}
+		
+		fis.close();
+		s.close();
+	}
 }
+
+
--- net/UploadServer.java
+++ net/UploadServer.java
@@ -1,8 +1,10 @@
 /**
- * CopyRight 2019/4/16
+ * CopyRight 2019/4/25
  */
 package cn.edu.tit.corejava.socket;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,7 +16,7 @@ import java.net.Socket;
 
 /**
  * @author 李创博
- * @version 1.2
+ * @version 1.3
  */
 public class UploadServer {
 
@@ -25,15 +27,19 @@ public class UploadServer {
 		/**流程：
 		 * 1.创建ServerSocket对象，绑定到指定端口号
 		 * 2.获得客户端对象Socket
-		 * 3.获得InputStream对象，并读入客户端发送的数据
-		 * 4.创建FileWriter对象，绑定数据源
-		 * 5.使用FileWriter将读入数据写入服务器硬盘
-		 *  
-		 * 6.获得OutputStream对象，回复客户端
-		 * 7.释放资源
-		 * 服务器端：21199字节
+		 * 3.判断传入的文件类型，根据不同的类型选择对应的接收方式
+		 * 4.获得InputStream对象，并读入客户端发送的数据
+		 * 5.创建FileWriter/FileOutputStream实例化对象，绑定数据源
+		 * 6.使用FileWriter/FileOutputStream将读入数据写入服务器硬盘 
+		 * 7.获得OutputStream对象，回复客户端
+		 * 8.释放资源
 		 * */	
 		ServerSocket ss = new ServerSocket(8000);
+		
+		File f = new File("D:\\upload\\");
+		if(!f.exists()) {
+			f.mkdirs();
+		}
 		
 		while(true) {
 			System.out.println("正在监听8000端口...");
@@ -46,27 +52,71 @@ public class UploadServer {
 					// 重写run方法，子类不能破坏父类的结构，而父类run方法不抛出异常，所以子类也不能抛出异常
 					try {
 						InputStream is = socket.getInputStream();
-						InputStreamReader isr = new InputStreamReader(is);
-						
-						String fileName = System.currentTimeMillis() + ".txt";
-						FileWriter fw = new FileWriter("D:\\upload\\" + fileName);
-						
-						int len = 0;
-						char []temp = new char[1024];
-						while((len = isr.read(temp)) != -1) {
-							fw.write(new String(temp, 0, len));
+						int num = is.read();
+						if(num == 1) {
+							loadUnTextFile(f, socket);			
+						}else if(num == 0) {
+							loadTextFile(f, socket);
 						}
-						
-						OutputStream os = socket.getOutputStream();
-						os.write("上传成功！".getBytes());	
-						
-						fw.close();
-						socket.close();
+						is.close();
 					}catch(IOException e) {
 						System.out.println(e);
 					}
 				}
 			}).start();
 		}			
+	}
+	
+	/**
+	 * 接收文本类型文件
+	 * @param f 存放文件的文件夹
+	 * @param s Socket对象
+	 * @throws IOException
+	 * @return: void
+	 */
+	public static void loadTextFile(File f, Socket s) throws IOException {
+		InputStream is = s.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		
+		String fileName = System.currentTimeMillis() + ".txt";
+		FileWriter fw = new FileWriter(f + "\\" + fileName);
+		
+		int len = 0;
+		char []temp = new char[1024];
+		while((len = isr.read(temp)) != -1) {
+			fw.write(new String(temp, 0, len));
+		}
+		
+		OutputStream os = s.getOutputStream();
+		os.write("上传成功！".getBytes());	
+		
+		fw.close();
+		s.close();
+	}
+	
+	/**
+	 * 接收非文本类型文件
+	 * @param f 存放文件的文件夹
+	 * @param s Socket对象
+	 * @throws IOException
+	 * @return: void
+	 */
+	public static void loadUnTextFile(File f, Socket s) throws IOException {
+		InputStream is = s.getInputStream();
+		
+		String fileName = System.currentTimeMillis() + ".jpg";
+		FileOutputStream fos = new FileOutputStream(f + "\\" + fileName);
+		
+		int len = 0;
+		byte []temp = new byte[1024];
+		while((len = is.read(temp)) != -1) {
+			fos.write(temp, 0, len);
+		}
+		
+		OutputStream os = s.getOutputStream();
+		os.write("上传成功！".getBytes());	
+		
+		fos.close();
+		s.close();
 	}
 }
