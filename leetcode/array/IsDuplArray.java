fc632cffde8f5d57d233900cc09ffb5e8d8ce1b7
xiaotian <15503609389@163.com>
2019-04-28 14:44:51 +0800

    Add other two effective solutions

--- leetcode/array/IsDuplArray.java
+++ leetcode/array/IsDuplArray.java
@@ -5,6 +5,8 @@
  */
 package cn.edu.tit.leetcode.array;
 
+import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * 判断数组是否含有重复元素
@@ -14,8 +16,8 @@ package cn.edu.tit.leetcode.array;
 public class IsDuplArray {
 	public boolean containsDuplicate(int[] nums) {
 		/**
+		 * 普通的双重循环
 		 * 测试用时423ms
-		 * flag	超时
 		 */
         for (int i = 0; i < nums.length; i++) {
 			int temp = nums[i];
@@ -29,21 +31,37 @@ public class IsDuplArray {
     }
 	
 	public boolean containDuplicate1(int []nums) {
-		int flag = 0;
-		for (int i = 1; i < nums.length; i++) {
-			int temp = nums[i];
-			if (nums[flag] == temp) {
-//				flag++;
+		/**
+		 * 先将数组进行排序，之后判断相邻数组是否相同即可
+		 * 测试用时：10ms
+		 */
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == nums[i + 1]) {
 				return true;
 			}
-//			flag++;
+		}
+		return false;
+	}
+	
+	public boolean containDuplicate2(int []nums) {
+		/**
+		 * 利用HashSet的不存储重复元素特性，判断set的大小和nums的长度是否一致
+		 * 测试用时：22ms
+		 */
+		HashSet<Integer> set = new HashSet<>();
+		for (int i = 0; i < nums.length; i++) {
+			set.add(nums[i]);
+		}
+		if (set.size() != nums.length) {
+			return true;
 		}
 		return false;
 	}
 	
 	public static void main(String[] args) {
-		int []nums = {1, 3, 3, 4};// 1234 false  1231true  1334true
+		int []nums = {1, 3, 3, 1};// 1234 false  1231true  1334true
 		IsDuplArray ida = new IsDuplArray();
-		System.out.println(ida.containDuplicate1(nums));
+		System.out.println(ida.containDuplicate2(nums));
 	}
 }
