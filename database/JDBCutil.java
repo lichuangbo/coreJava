1096a5fc9ee78e72e85ef7673bc23fecafc11c08
xiaotian <15503609389@163.com>
2019-05-13 21:20:19 +0800

    JDBC connect MySQL util

---
+++ database/JDBCutil.java
@@ -0,0 +1,98 @@
+/**
+ * CopyRight © 2019All rights reserved.
+ * 
+ * @date: 2019年5月13日
+ */
+package cn.edu.tit.corejava.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ * JDBC工具类
+ * @author 李创博
+ * @version: 1.0
+ */
+public class JDBCutil {
+	private static final String USER = "root";
+	private static final String PWD = "root";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/test";
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	
+	static {
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			System.out.println(e);
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL, USER, PWD);
+	}
+	
+	/**
+	 * 释放打开的资源
+	 * @param conn Connection
+	 * @param st Statement
+	 * @return: void
+	 */
+	public static void close(Connection conn, Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 释放打开的资源
+	 * @param conn Connection
+	 * @param st Statement
+	 * @param rs ResultSet
+	 * @return: void
+	 */
+	public static void close(Connection conn, Statement st, ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		close(conn, st);
+	}
+	
+	/**
+	 * 释放打开的资源
+	 * @param conn Connection
+	 * @param st Connection
+	 * @param rs ResultSet
+	 * @param ps PreparedStatement
+	 * @return: void
+	 */
+	public static void close(Connection conn, Statement st, ResultSet rs, PreparedStatement ps) {
+		if (ps != null) {
+			try {
+				ps.close();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		close(conn, st, rs);
+	} 
+}
